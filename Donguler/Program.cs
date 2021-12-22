70f3cd9a610b90d3a5403c524a666110186d817d
cemeroksuz <cemeroksuz@gmail.com>
2021-12-22 18:08:40 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+string kurs2 = "Proramlamaya Başlangıç İçin Temel Kurs";
+string kurs3 = "Java";
+string kurs4 = "Python";
+string kurs5 = "C++";
+
+string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Proramlamaya Başlangıç İçin Temel Kurs", "Java", "Python","C++" };
+
+for (int i = 0; i < kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+} 
+
+Console.WriteLine("----------------------");
+foreach(string kurs in kurslar)
+{
+    Console.WriteLine(kurs);
+}
+
+Console.WriteLine("----------------------");
+
+Console.WriteLine("Sayfa Sonu");    
---
+++ KampInrto/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string kategoriEtiketi = "Kategoriler";
+int ogrenciSayisi = 32000;
+double faizOrani = 1.45;
+bool sistemeGirisYapmisMi = true;
+double dolarDun = 7.35;
+double dolarBugun = 7.45;
+
+if (dolarDun > dolarBugun)
+{
+    Console.WriteLine("Azalış Butonu");
+}
+else if(dolarDun < dolarBugun)
+{
+    Console.WriteLine("Artış Butonu");
+}
+else
+{
+    Console.WriteLine("Eşittir Butonu");
+}
+
+
+if (sistemeGirisYapmisMi == true)
+{   
+    Console.WriteLine("Kullanıcı Ayarları Butonu");
+}
+else
+{
+    Console.WriteLine("Giriş Yap Butonu");
+}   
+
+
+Console.WriteLine(kategoriEtiketi);
